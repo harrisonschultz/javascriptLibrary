8ab6d3474f5c003c4fbbe46a6a9720c6507594c9
Harrison Schultz <hschultz1337@gmail.com>
2017-06-19 10:07:16 -0400

    added callbacksj.js

---
+++ jsFundamentals/0.09_callbacks.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+
+/*
+
+    request = prepare_the_request()
+    send_request_asynchronously (request, function (response){
+        display(response);
+    });
+
+*/
+
+function getBulls(filePath, done){
+    //telling file system to read a file that the user of the function passes into 
+    // as a argument. Then we are creating a callback to handle that case.
+    fs.readFile(filePath, function(err, bulls){
+        //error handling
+        if (err) return done(err);
+
+        fs.readFile('0.09_bullsdictionary.txt', function(err, dictionary){
+            //error handling
+            if (err) return done(err);
+
+            //Do some logic here
+            compareBulls(bulls, dictionary);
+        })
+    })
+    //variable way
+var compareBulls = function(bulls, dict){
+    //turn items from these files into elements in an array then split them
+    dict = dict.toString().split('\n');
+    bulls = bulls.toString().split('\n').filter(function(bulls){
+       // iterating over the dictionary, then it's checking if the bulls ar located
+       // inside of the dictionary. If it is then it's returning that value.
+        return dict.indexOf(bulls) !== -1;
+    });
+    done(bulls);
+};
+}
+
+
+getBulls('0.09_bulls.txt',function(bulls){
+
+    console.log(bulls);
+});
--- jsFundamentals/fdsa.js
+++
@@ -1,3 +0,0 @@
-function checkTest(){
-	test();
-}
